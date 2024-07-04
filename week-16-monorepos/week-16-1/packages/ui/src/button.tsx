a57cbfd914db3738e9fe1457ef1b868f7f5a955c
Atharva <deshmukhsambhajirao9@gmail.com>
2024-07-04 15:01:09 +0530

    added 16.1

---
+++ week-16-monorepos/week-16-1/packages/ui/src/button.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Button = () => {
+  return (
+    <button
+      className=""
+      onClick={() => alert(`Hello from your app!`)}
+    >
+      Hi from button
+    </button>
+  );
+};
