66504536378f15bde80d4b3d25f9afaec15892b0
Atharva <deshmukhsambhajirao9@gmail.com>
2024-09-23 22:54:59 +0530

    new workflow test

--- week-18/week-18-2-ci-cd/apps/user-app/app/page.tsx
+++ week-18/week-18-2-ci-cd/apps/user-app/app/page.tsx
@@ -5,6 +5,8 @@ import { authOptions } from "./lib/auth";
 export default async function Page() {
 
   // making changes to check if the workflow is working
+
+  // another check comment
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
