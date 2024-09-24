ca3cbc4e81024bd90b7d304789d5a41cb5880c0d
Atharva <deshmukhsambhajirao9@gmail.com>
2024-09-24 12:04:49 +0530

    checking the deployment workflow with dockerfile

--- week-18/week-18-2-ci-cd/apps/user-app/app/page.tsx
+++ week-18/week-18-2-ci-cd/apps/user-app/app/page.tsx
@@ -5,6 +5,9 @@ import { authOptions } from "./lib/auth";
 export default async function Page() {
 
   // making changes to check if the workflow is working
+
+
+  // making changes to check if the workflow is working
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
